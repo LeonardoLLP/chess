26bfc53a4cfa0b9cfb27b5ba840212a1f4ca56e8
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2021-12-10 00:30:57 +0100

    Added characters for all off white pieces

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Chess
 Chess game
+
+(Unicode list of pieces)[https://unicode-table.com/en/sets/chess-symbols/]: https://unicode-table.com/en/sets/chess-symbols/
--- chess.py
+++ chess.py
@@ -2,7 +2,15 @@ rows = 8
 columns = 8
 
 # Queen unicode: U+2655
-white_queen = chr(0x2655)
+piece = {
+    "white_king": chr(0x2654),
+    "white_queen": chr(0x2655),
+    "white_rook": chr(0x2656),
+    "white_bishop": chr(0x2657),
+    "white_knight": chr(0x2658),
+    "white_pawn": chr(0x2659)
+}
 
 
-print(white_queen)
+
+print(piece)
