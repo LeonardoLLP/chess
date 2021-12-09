f12de4a038293723f677e0fe7553039f7e262200
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2021-12-10 00:39:45 +0100

    Added black pieces and separated both dictionaries

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Chess
 Chess game
 
-[Unicode list of pieces](https://unicode-table.com/en/sets/chess-symbols/): https://unicode-table.com/en/sets/chess-symbols/
+[Unicode list of pieces](https://unicode-table.com/en/sets/chess-symbols/)
--- chess.py
+++ chess.py
@@ -2,15 +2,29 @@ rows = 8
 columns = 8
 
 # Queen unicode: U+2655
-piece = {
-    "white_king": chr(0x2654),
-    "white_queen": chr(0x2655),
-    "white_rook": chr(0x2656),
-    "white_bishop": chr(0x2657),
-    "white_knight": chr(0x2658),
-    "white_pawn": chr(0x2659)
+white = {
+    "king": chr(0x2654),
+    "queen": chr(0x2655),
+    "rook": chr(0x2656),
+    "bishop": chr(0x2657),
+    "knight": chr(0x2658),
+    "pawn": chr(0x2659)
 }
 
+black = {
+    "king": chr(0x265A),
+    "queen": chr(0x265B),
+    "rook": chr(0x265C),
+    "bishop": chr(0x265D),
+    "knight": chr(0x265E),
+    "pawn": chr(0x265F)
+}
+
+# in case I need it later
+checkered_board = chr(0x1F67E)
 
 
-print(piece)
+
+print(white)
+print(black)
+print(checkered_board)
