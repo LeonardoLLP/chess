db025f2ecba004c21abc045c7461578e271d0b8f
Leonardo Luque <64507345+LeonardoLLP@users.noreply.github.com>
2021-12-13 15:46:21 +0100

    Reutilizado código de movimiento de alfil para torre y reina Falta optimizización

--- chess.py
+++ chess.py
@@ -50,9 +50,6 @@ def add_pieces(places: list, piece: str):
     for place in places:
         board[place[0]-1][place[1]-1] = piece
 
-
-
-
 """Move piece, independently if it can or not (check done in is_valid function)"""
 def move_piece(origin_: list, dest_: list, piece: str):
     board[origin_[0]][origin_[1]] = ""
@@ -78,12 +75,14 @@ def is_valid(move_: list, dest_: list, piece_: str, colour_list_: list):
         x_s = int(x / abs(x))
     else:
         x_s = 0  # x sign
+
     if y != 0:
         y_s = int(y / abs(y))
     else:
         y_s = 0
 
     cannival = board[dest_[0]][dest_[1]] in colour_list_
+
 
     if move_ == [0, 0]:  # Innecesario: cannival da verdadero ya que es su misma casilla
         return False
@@ -107,7 +106,7 @@ def is_valid(move_: list, dest_: list, piece_: str, colour_list_: list):
 
     # Si me preguntas no entiendo ni yo que cojones estoy haciendo.
     # El programa INTENTA iterar por cada posición que tiene que pasar el alfil a ver si está libre. DEBERÍA DE FUNCIONAR.
-    # TODO: Test
+    # TODO: OPTIMIZAR la repetición de código
     elif piece_str == "bishop":
         if abs(x) == abs(y):
             places_to_pass = [(i * y_s, i * x_s) for i in range(1, abs(x))]
@@ -123,10 +122,32 @@ def is_valid(move_: list, dest_: list, piece_: str, colour_list_: list):
 
 
     elif piece_str == "rook":
-        pass
+        if x == 0 or y == 0:
+            places_to_pass = [(i * y_s, i * x_s) for i in range(1, abs(x))]
+            for place in places_to_pass:
+                try:
+                    if board[dest_[0]-place[0]][dest_[1]-place[1]] != "":
+                        return False
+                except:
+                    return False
+            return True
+        else:
+            return False
+
 
     elif piece_str == "queen":
-        pass
+        if x == 0 or y == 0 or abs(x) == abs(y):
+            places_to_pass = [(i * y_s, i * x_s) for i in range(1, abs(x))]
+            for place in places_to_pass:
+                try:
+                    if board[dest_[0]-place[0]][dest_[1]-place[1]] != "":
+                        return False
+                except:
+                    return False
+            return True
+        else:
+            return False
+
 
     elif piece_str == "pawn":
         pass
